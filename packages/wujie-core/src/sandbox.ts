8b2058fef706a29692322bf99565f78228a6c0e6
EdwardYang <edward-yang1109@hotmail.com>
2025-08-21 14:51:11 +0800

    fix: 先修复刷新子应用报错问题

--- packages/wujie-core/src/sandbox.ts
+++ packages/wujie-core/src/sandbox.ts
@@ -33,8 +33,8 @@ import { EventBus, appEventObjMap, EventObj } from "./event";
 import { isFunction, wujieSupport, appRouteParse, requestIdleCallback, getAbsolutePath, eventTrigger } from "./utils";
 import { WUJIE_DATA_ATTACH_CSS_FLAG } from "./constant";
 import { plugin, ScriptObjectLoader, loadErrorHandler } from "./index";
-import { clearAllCache } from "./entry";
-import { clearRawDocumentQuerySelector } from "./common";
+// import { clearAllCache } from "./entry";
+// import { clearRawDocumentQuerySelector } from "./common";
 
 export type lifecycle = (appWindow: Window) => any;
 type lifecycles = {
@@ -400,8 +400,8 @@ export default class Wujie {
   /** 销毁子应用 */
   public destroy() {
     this.unmount();
-    clearAllCache();
-    clearRawDocumentQuerySelector();
+    // clearAllCache();
+    // clearRawDocumentQuerySelector();
     this.bus.$clear();
     this.shadowRoot = null;
     this.proxy = null;
